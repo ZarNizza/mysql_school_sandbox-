fe718544e095ebe451610bdd3bb2e04b91b0b84f
ZarNizza <gormaster@mail.ru>
2022-12-10 23:39:37 +0400

    pg_structure

--- db_structure_pg.sql
+++ db_structure_pg.sql
@@ -17,21 +17,22 @@ INSERT INTO classes (grade, symbol, chief, size) VALUES (1, 'Б', 2, 20);
 INSERT INTO subjects (grade, name) VALUES (1, 'Химия');
 INSERT INTO subjects (grade, name) VALUES (1, 'Физика');
 
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Рыбников', 'Иннокентий', 'Петрович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Линдгрен', 'Фрекен', 'Бок');
+INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Рыбников', 'Иннокентий', 'Петрович'),('Линдгрен', 'Фрекен', 'Бок');
 
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 1, 15);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 2, 5);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 1, 15);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 2, 5);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 1, 15);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 1, 2, 10);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 1, 10);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 2, 20);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 1, 10);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 2, 20);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 1, 10);
-INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 2, 20);
+INSERT INTO log (time_slot, class, subj, tutor, size)
+VALUES
+(1, 1, 1, 1, 15),
+(1, 1, 1, 2, 5),
+(1, 1, 1, 1, 15),
+(1, 1, 1, 2, 5),
+(1, 1, 1, 1, 15),
+(1, 1, 1, 2, 10),
+(1, 1, 2, 1, 10),
+(1, 1, 2, 2, 20),
+(1, 1, 2, 1, 10),
+(1, 1, 2, 2, 20),
+(1, 1, 2, 1, 10),
+(1, 1, 2, 2, 20);
 
 # ожидаемый результат посещаемости по учителям:
 # 1 (Рыбников Иннокентий Петрович) - 62,5%
@@ -56,24 +57,25 @@ INSERT INTO log (time_slot, class, subj, tutor, size) VALUES (1, 1, 2, 2, 20);
 #
 # наполняем таблицу subjects
 #
-INSERT INTO subjects (grade, name) VALUES (1, 'Сказки');
-INSERT INTO subjects (grade, name) VALUES (1, 'Каляки-маляки');
-INSERT INTO subjects (grade, name) VALUES (1, 'Гуляшки');
-INSERT INTO subjects (grade, name) VALUES (2, 'Правописание');
-INSERT INTO subjects (grade, name) VALUES (2, 'ЛевоЧитание');
-INSERT INTO subjects (grade, name) VALUES (2, 'ФизКультура');
-INSERT INTO subjects (grade, name) VALUES (3, 'Чтение');
-INSERT INTO subjects (grade, name) VALUES (3, 'ФизКультура');
-INSERT INTO subjects (grade, name) VALUES (3, 'Лепка');
-INSERT INTO subjects (grade, name) VALUES (4, 'Природоведение');
-INSERT INTO subjects (grade, name) VALUES (4, 'Кругозор');
-INSERT INTO subjects (grade, name) VALUES (4, 'ФизКультура');
-INSERT INTO subjects (grade, name) VALUES (5, 'История');
-INSERT INTO subjects (grade, name) VALUES (5, 'ФизКультура');
-INSERT INTO subjects (grade, name) VALUES (5, 'Математика');
-INSERT INTO subjects (grade, name) VALUES (6, 'Физика');
-INSERT INTO subjects (grade, name) VALUES (6, 'Химия');
-INSERT INTO subjects (grade, name) VALUES (6, 'ФизКультура');
+INSERT INTO subjects (grade, name) VALUES
+(1, 'Сказки'),
+(1, 'Каляки-маляки'),
+(1, 'Гуляшки'),
+(2, 'Правописание'),
+(2, 'ЛевоЧитание'),
+(2, 'ФизКультура'),
+(3, 'Чтение'),
+(3, 'ФизКультура'),
+(3, 'Лепка'),
+(4, 'Природоведение'),
+(4, 'Кругозор'),
+(4, 'ФизКультура'),
+(5, 'История'),
+(5, 'ФизКультура'),
+(5, 'Математика'),
+(6, 'Физика'),
+(6, 'Химия'),
+(6, 'ФизКультура');
 
 
 # наполняем таблицу classes
@@ -81,62 +83,63 @@ INSERT INTO subjects (grade, name) VALUES (6, 'ФизКультура');
 # вручную заполнять лень — пишем процедуру
 #
 DROP PROCEDURE IF EXISTS fill_class;
-DELIMITER //
-CREATE PROCEDURE fill_class()
+
+CREATE OR REPLACE PROCEDURE fill_class() as
+$$
+DECLARE
+    v_grade INT := 11;
 BEGIN
-  DECLARE v_grade INT UNSIGNED DEFAULT 11;
   TRUNCATE TABLE classes;
-  START TRANSACTION;
-  WHILE v_grade DO
-	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'А', v_grade*3, ROUND(RAND()*15)+10);
-	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'Б', v_grade*3-1, ROUND(RAND()*15)+10);
-	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'В', v_grade*3-2, ROUND(RAND()*15)+10);
-  SET v_grade = v_grade - 1;
- END WHILE;
- COMMIT;
+  WHILE v_grade > 0 loop
+	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'А', v_grade*3, ROUND(RANDOM()*15)+10);
+	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'Б', v_grade*3-1, ROUND(RANDOM()*15)+10);
+	INSERT INTO classes (grade, symbol, chief, size) VALUES (v_grade, 'В', v_grade*3-2, ROUND(RANDOM()*15)+10);
+    v_grade := v_grade - 1;
+ END loop;
 END
-//
-DELIMITER ;
+$$ language plpgsql;
+
 CALL fill_class();
 
 
 # наполняем таблицу tutors
 #
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Матвеева', 'Арина', 'Родионовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Рыбников', 'Иннокентий', 'Петрович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Бардина', 'Чуча', '');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Линдгрен', 'Фрекен', 'Бок');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Поппинс', 'Мэри', 'Памела');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Достоевская', 'Алёна', 'Фроловна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Пельтцер', 'Татьяна', 'Ивановна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Прутковская', 'Вика', '');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Жиакомо', 'Жиан', '');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Бальзамо', 'Джузеппе', 'Петрович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Киврин', 'Фёдор', 'Симеонович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Хунта', 'Кристобаль', 'Хозевич');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Анна', 'Борисовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Борислава', 'Витальевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Валентина', 'Геннадьевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Галина', 'Демьяновна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Дарья', 'Евгеньевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Евгения', 'Жиановна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Жанна', 'Зиновьевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Зейнаб', 'Ибрагимовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Илона', 'Йоновна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Йожи', 'Кальмановна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Катрин', 'Лемовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Лайма', 'Максимовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Мария', 'Николаевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Нинель', 'Онуфриевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Оксана', 'Порфирьевна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Павел', 'Романович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Роксана', 'Станиславовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Сильвестр', 'Сталлонович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Светлана', 'Тарасовна');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Терентий', 'Ульянович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Уфим', 'Фёдорович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Фаддей', 'Харитонович');
-INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', 'Хельга', 'Церберовна');
+INSERT INTO tutors (lastName, firstName, patronym) VALUES
+('Матвеева', 'Арина', 'Родионовна'),
+('Рыбников', 'Иннокентий', 'Петрович'),
+('Бардина', 'Чуча', ''),
+('Линдгрен', 'Фрекен', 'Бок'),
+('Поппинс', 'Мэри', 'Памела'),
+('Достоевская', 'Алёна', 'Фроловна'),
+('Пельтцер', 'Татьяна', 'Ивановна'),
+('Прутковская', 'Вика', ''),
+('Жиакомо', 'Жиан', ''),
+('Бальзамо', 'Джузеппе', 'Петрович'),
+('Киврин', 'Фёдор', 'Симеонович'),
+('Хунта', 'Кристобаль', 'Хозевич'),
+('Яндексон', 'Анна', 'Борисовна'),
+('Яндексон', 'Борислава', 'Витальевна'),
+('Яндексон', 'Валентина', 'Геннадьевна'),
+('Яндексон', 'Галина', 'Демьяновна'),
+('Яндексон', 'Дарья', 'Евгеньевна'),
+('Яндексон', 'Евгения', 'Жиановна'),
+('Яндексон', 'Жанна', 'Зиновьевна'),
+('Яндексон', 'Зейнаб', 'Ибрагимовна'),
+('Яндексон', 'Илона', 'Йоновна'),
+('Яндексон', 'Йожи', 'Кальмановна'),
+('Яндексон', 'Катрин', 'Лемовна'),
+('Яндексон', 'Лайма', 'Максимовна'),
+('Яндексон', 'Мария', 'Николаевна'),
+('Яндексон', 'Нинель', 'Онуфриевна'),
+('Яндексон', 'Оксана', 'Порфирьевна'),
+('Яндексон', 'Павел', 'Романович'),
+('Яндексон', 'Роксана', 'Станиславовна'),
+('Яндексон', 'Сильвестр', 'Сталлонович'),
+('Яндексон', 'Светлана', 'Тарасовна'),
+('Яндексон', 'Терентий', 'Ульянович'),
+('Яндексон', 'Уфим', 'Фёдорович'),
+('Яндексон', 'Фаддей', 'Харитонович'),
+('Яндексон', 'Хельга', 'Церберовна');
 
 
 # наполняем таблицу log
@@ -147,34 +150,33 @@ INSERT INTO tutors (lastName, firstName, patronym) VALUES ('Яндексон', '
 #  - предметы и учителя задаются случайно, первому классу может выпасть урок для 11го, но для наших целей реалистичность не так важна
 #
 DROP PROCEDURE IF EXISTS fill_log;
-DELIMITER //
-CREATE PROCEDURE fill_log()
+
+CREATE OR REPLACE PROCEDURE fill_log() as
+$$
+    DECLARE curr_date DATE := CURRENT_DATE;
+    DECLARE log_date DATE := CURRENT_DATE - interval '10 days';
+    DECLARE time_slot SMALLINT := 1;
+    DECLARE class SMALLINT := 1;
+    DECLARE subj_num SMALLINT := (SELECT COUNT(*) FROM subjects);
+    DECLARE tutor_num SMALLINT := (SELECT COUNT(*) FROM tutors);
+
 BEGIN
-    DECLARE curr_date DATE DEFAULT (CURRENT_DATE);
-    DECLARE log_date DATE DEFAULT SUBDATE(CURRENT_DATE, 10);
-    DECLARE time_slot SMALLINT DEFAULT 1;
-    DECLARE class SMALLINT DEFAULT 1;
-    DECLARE subj_num SMALLINT DEFAULT (SELECT COUNT(*) FROM subjects);
-    DECLARE tutor_num SMALLINT DEFAULT (SELECT COUNT(*) FROM tutors);
-
     TRUNCATE TABLE log;
-    START TRANSACTION;
 
-    WHILE log_date <= curr_date DO
-        WHILE time_slot <= 5 DO
-            WHILE class <= 33 DO
-                INSERT INTO log (log_date, time_slot, class, subj, tutor, size) VALUES (log_date, time_slot, class, ROUND(1 + RAND() * (subj_num - 1)), ROUND(1 + RAND() * (tutor_num - 1)), ROUND(RAND() * (SELECT size FROM classes WHERE id = class)));
-                SET class = class + 1;
-            END WHILE;
-            SET class = 1;
-            SET time_slot = time_slot + 1;
-        END WHILE;
-        SET time_slot = 1;
-        SET log_date = DATE_ADD(log_date, INTERVAL 1 DAY);
-    END WHILE;
-    COMMIT;
+    WHILE log_date <= curr_date LOOP
+        WHILE time_slot <= 5 LOOP
+            WHILE class <= 33 LOOP
+                INSERT INTO log (log_date, time_slot, class, subj, tutor, size) VALUES (log_date, time_slot, class, ROUND(1 + RANDOM() * (subj_num - 1)), ROUND(1 + RANDOM() * (tutor_num - 1)), ROUND(RANDOM() * (SELECT size FROM classes WHERE id = class)));
+                class := class + 1;
+            END LOOP;
+            class := 1;
+            time_slot := time_slot + 1;
+        END LOOP;
+        time_slot := 1;
+        log_date:= log_date + INTERVAL '1 DAY';
+    END LOOP;
 END
-//
-DELIMITER ;
+$$ language plpgsql;
+
 CALL fill_log();
 
